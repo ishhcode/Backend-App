04cbf859410ccc3075b138b34181d1b326279efa
Ishika Aggarwal <ishikaaggarwal003@gmail.com>
2024-02-02 18:50:19 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRoutes from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/users",userRoutes);
+
+//    http://localhost:8000/api/users/register
+
 
 //export default app;
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
-const asyncHandler = async(requestHandler)=>(req,res,next)=>{
-     // Promise.resolve() is used to convert non-Promise values to Promises
-    // This allows the requestHandler to be both synchronous and asynchronous
+const asyncHandler = (requestHandler) =>
+    {return(req, res, next) => {
+        // Promise.resolve() is used to convert non-Promise values to Promises
+        // This allows the requestHandler to be both synchronous and asynchronous
 
-    // Execute the requestHandler function and handle any errors
-    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-};
+        // Execute the requestHandler function and handle any errors
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }};
 
 export { asyncHandler }
 
